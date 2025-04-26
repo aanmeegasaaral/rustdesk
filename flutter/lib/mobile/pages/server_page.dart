a72c50716f4124fd0ba897eb5925e01ec4b7f14e
Varun Sridharan <varunsridharan23@gmail.com>
2025-04-26 09:04:56 +0530

    updated

--- flutter/lib/mobile/pages/server_page.dart
+++ flutter/lib/mobile/pages/server_page.dart
@@ -232,13 +232,7 @@ class ServiceNotRunningNotification extends StatelessWidget {
             ElevatedButton.icon(
                 icon: const Icon(Icons.play_arrow),
                 onPressed: () {
-                  if (gFFI.userModel.userName.value.isEmpty &&
-                      bind.mainGetLocalOption(key: "show-scam-warning") !=
-                          "N") {
-                    showScamWarning(context, serverModel);
-                  } else {
-                    serverModel.toggleService();
-                  }
+                  serverModel.toggleService();
                 },
                 label: Text(translate("Start service")))
           ],
@@ -574,18 +568,9 @@ class _PermissionCheckerState extends State<PermissionChecker> {
                       label: Text(translate("Stop service")))
                   .marginOnly(bottom: 8)
               : SizedBox.shrink(),
-          PermissionRow(
-              translate("Screen Capture"),
-              serverModel.mediaOk,
-              !serverModel.mediaOk &&
-                      gFFI.userModel.userName.value.isEmpty &&
-                      bind.mainGetLocalOption(key: "show-scam-warning") != "N"
-                  ? () => showScamWarning(context, serverModel)
-                  : serverModel.toggleService),
-          PermissionRow(translate("Input Control"), serverModel.inputOk,
-              serverModel.toggleInput),
-          PermissionRow(translate("Transfer file"), serverModel.fileOk,
-              serverModel.toggleFile),
+          PermissionRow( translate("Screen Capture"), serverModel.mediaOk, serverModel.toggleService),
+          PermissionRow(translate("Input Control"), serverModel.inputOk, serverModel.toggleInput),
+          PermissionRow(translate("Transfer file"), serverModel.fileOk, serverModel.toggleFile),
           hasAudioPermission
               ? PermissionRow(translate("Audio Capture"), serverModel.audioOk,
                   serverModel.toggleAudio)
