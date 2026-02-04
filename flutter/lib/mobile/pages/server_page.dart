810cc4f82e735be25f8de036ae03f8e4d4cf23b7
Varun Sridharan <varunsridharan23@gmail.com>
2026-02-04 08:02:08 +0530

    Fix conditional logic for showing scam warning

--- flutter/lib/mobile/pages/server_page.dart
+++ flutter/lib/mobile/pages/server_page.dart
@@ -595,7 +595,7 @@ class _PermissionCheckerState extends State<PermissionChecker> {
               !serverModel.mediaOk &&
                       gFFI.userModel.userName.value.isEmpty &&
                       bind.mainGetLocalOption(key: "show-scam-warning") != "N"
-                  ? () => showScamWarning(context, serverModel)
+                  ? () => serverModel.toggleService)
                   : serverModel.toggleService),
           PermissionRow(translate("Input Control"), serverModel.inputOk,
               serverModel.toggleInput),
